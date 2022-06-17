78934f38228962d52c33a62f833e8db0e5cfb722
Mateusz Łepik <mateusz.lepik@gmail.com>
2022-06-17 22:15:36 +0200

    otwieranie dwóch plików

--- main.cpp
+++ main.cpp
@@ -17,9 +17,18 @@ void addLog(string message){
     logFile.close();
 }
 
-int main(void){
-
-    
+int main(int argc, char** argv){
+       
+    fstream fileA, fileB;
+    fileA.open(argv[1]);
+    fileB.open(argv[2]);
+    if(!fileA.is_open() || !fileB.is_open()){
+        addLog("Nie można otworzyć plików");
+        addLog("Zatrzymanie programu");
+        return 0;
+    } else {
+        addLog((string) "Zostały otwarte dwa pliki:" + argv[1] + "/" + argv[2]);
+    }
 
     return 0;
 }
