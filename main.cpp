b04945d655dc4017ad10c6cbf760a89c011d6e3d
Mateusz Łepik <mateusz.lepik@gmail.com>
2022-06-17 21:51:00 +0200

    dodanie funkcji addLog

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@
 
 using namespace std;
 
+void addLog(string message){
+    fstream logFile;
+    logFile.open("logi.txt", ios::app);
+    time_t t = time(0);
+    string dt = ctime(&t);
+    logFile << endl << dt.substr(0, dt.length()-1) << " : " << message;
+    logFile.close();
+}
 
 int main(void){
 
